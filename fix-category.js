fb3c3a71e686f13d7472ea5edf8e684808cfc35b
AliHaider728 <decantboy67@gmail.com>
2026-09-27 00:09:03 +0500

    chore: remove script files

--- fix-category.js
+++
@@ -1,4 +0,0 @@
-﻿const fs = require('fs');
-let text = fs.readFileSync('src/app/category/[slug]/page.tsx', 'utf8');
-text = text.replace(/title: 'All Products '/g, "title: 'All Products'");
-fs.writeFileSync('src/app/category/[slug]/page.tsx', text);
